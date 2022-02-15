07df41008b07caeff2fe891a84f652d83a912133
Raja Babu <18283763+therajababu@users.noreply.github.com>
2022-02-15 18:16:32 +0530

    Add JS script

---
+++ assets/scripts/app.js
@@ -0,0 +1,72 @@
+function onLoginSubmitHandler() {
+
+    var email = document.forms["loginForm"]["email"].value;
+    var password = document.forms["loginForm"]["password"].value;
+
+    // console.log(email);
+    // console.log(password);
+
+    if (email == "" && password == "") {
+        alert("Email & Password can't be blank, must be filled out!");
+        return false;
+    } else if (email == "") {
+        alert("Email can't be blank, must be filled out!");
+        return false;
+    } else if (password == "") {
+        alert("Password can't be blank, must be filled out!");
+        return false;
+    } else if (email != "") {
+
+        var aPos = email.indexOf("@");
+        var dotPos = email.lastIndexOf(".");
+
+        if (aPos < 1 || dotPos - aPos < 2) {
+            alert("Please enter valid email id!");
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
+
+function validateRegisterFormSubmitHandler() {
+
+    var fullName = document.forms["registerForm"]["fullName"].value;
+    var email = document.forms["registerForm"]["email"].value;
+    var password = document.forms["registerForm"]["password"].value;
+    var confirmPassword = document.forms["registerForm"]["confirmPassword"].value;
+
+    console.log(fullName, email, password, confirmPassword);
+
+    if (fullName == "") {
+        alert("Name can't be blank, must be filled out!");
+        return false;
+    } else if (email == "") {
+        alert("Email can't be blank, must be filled out!");
+        return false;
+    } else if (password == "") {
+        alert("Password can't be blank, must be filled out!");
+        return false;
+    } else if (confirmPassword == "") {
+        alert("Confirm Password can't be blank, must be filled out!");
+        return false;
+    } else if (password !== confirmPassword) {
+        alert("Both password should match!");
+        return false;
+    } else if (email != "") {
+
+        var aPos = email.indexOf("@");
+        var dotPos = email.lastIndexOf(".");
+
+        if (aPos < 1 || dotPos - aPos < 2) {
+            alert("Please enter valid email id!");
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
