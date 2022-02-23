92b380aeaee8f41adbe67fa07d33e7a4a14f2af9
Raja Babu <rajababu1259@gmail.com>
2022-02-23 22:25:55 +0530

    Create isEmailvalid function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,15 @@
+function isEmailValid(email) {
+    var aPos = email.indexOf("@");
+    var dotPos = email.lastIndexOf(".");
+
+    if (aPos < 1 || dotPos - aPos < 2) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 function onLoginSubmitHandler() {
 
     var email = document.forms["loginForm"]["email"].value;
@@ -33,6 +45,12 @@ function onLoginSubmitHandler() {
 
 function validateRegisterFormSubmitHandler() {
 
+    // Getting user data
+    var getUsersFromLS = JSON.parse(localStorage.getItem('usersLS'));
+
+    var users = getUsersFromLS ? getUsersFromLS : [];
+    console.log("users", users);
+
     var fullName = document.forms["registerForm"]["fullName"].value;
     var email = document.forms["registerForm"]["email"].value;
     var password = document.forms["registerForm"]["password"].value;
@@ -55,18 +73,12 @@ function validateRegisterFormSubmitHandler() {
     } else if (password !== confirmPassword) {
         alert("Both password should match!");
         return false;
-    } else if (email != "") {
+    } else if (!isEmailValid) { // invalid email
+        alert("Please enter valid email id!");
+        return false;
+    } else { // If everything is valid
 
-        var aPos = email.indexOf("@");
-        var dotPos = email.lastIndexOf(".");
-
-        if (aPos < 1 || dotPos - aPos < 2) {
-            alert("Please enter valid email id!");
-            return false;
-        } else {
-            return true;
-        }
-    } else {
+        
         return true;
     }
 }
